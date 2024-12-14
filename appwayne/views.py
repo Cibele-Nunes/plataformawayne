15755eafc61f4f95f94ca5efe437bd2fd58c3c31
Cibele <aeneb2012@gmail.com>
2024-12-14 01:52:21 -0300

    adaptação de arquivos direcionadas para painel do visitante

--- appwayne/views.py
+++ appwayne/views.py
@@ -16,10 +16,14 @@ def register():
         return redirect(url_for('templates.login'))
     return render_template('register.html', form=form)
     
-@main_blueprint.route('/login', methods=['GET', 'POST'])
-def login():
+@main_blueprint.route('/login_visit', methods=['GET', 'POST'])
+def login_visit():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login bem sucedido!', 'sucess')
-        return redirect(url_for('templates.users'))
-    return render_template('login.html', form=form)
+        return redirect(url_for('templates.users_visit'))
+    return render_template('login_visit.html', form=form)
+
+@main_blueprint.route('/users_visit/<nome>')
+def users_visit(nome):
+    return render_template('users_visit.html', nome=nome)
