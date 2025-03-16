1faddad47dae6210ce91406c46765444de5eb487
Cibele <aeneb2012@gmail.com>
2025-03-16 00:48:20 -0300

    testando funcionamento do formulário de cadastro junto com bd.

--- appwayne/forms.py
+++ appwayne/forms.py
@@ -3,11 +3,11 @@ from wtforms import StringField, IntegerField, PasswordField, SubmitField, Radio
 from wtforms.validators import DataRequired, InputRequired, EqualTo, Email, Length
 
 class RegistrationForm(FlaskForm):
-    user_nome = StringField('Crie o seu nome de usuário', validators=[DataRequired(), Length(min=3, max=50)])
     nome = StringField('Digite o seu nome', validators=[DataRequired(), Length(min=3, max=50)])
     sobrenome = StringField('Seu sobrenome', validators=[Length(min=3, max=50)])
+    user_nome = StringField('Crie o seu nome de usuário', validators=[DataRequired(), Length(min=3, max=50)])
     email = StringField('E-mail', validators=[Email(message='E-mail inválido!'), DataRequired(), Length(min=4, max=50)])
-    senha = PasswordField('Crie uma senha', validators=[InputRequired(), EqualTo('confirm_senha', message='As senhas devem ser iguais'), Length(min=6, max=50)])
+    senha = PasswordField('Crie a sua senha', validators=[InputRequired(), EqualTo('confirm_senha', message='As senhas devem ser iguais'), Length(min=6, max=50)])
     confirm_senha = PasswordField('Confirme a sua senha', validators=[InputRequired(), Length(min=6, max=50)])
     newsletters = RadioField('Gostaria de receber nossas newsletters?', choices=[('Sim', 'Sim'), ('Não', 'Não')], validators=[DataRequired()])
     submit = SubmitField('Cadastrar')
